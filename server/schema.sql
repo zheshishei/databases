c23d7c5f22c296b5b55e0c30f78dbed958b0b8ea
zheshishei <asun0927@gmail.com>
2015-01-10 10:37:26 -0800

    Added to schema - primary keys, foreign keys, etc.

--- server/schema.sql
+++ server/schema.sql
@@ -3,26 +3,48 @@ CREATE DATABASE IF NOT EXISTS chat;
 USE chat;
 
 CREATE TABLE IF NOT EXISTS users (
-  ID          INT(8),
-  username    VARCHAR(15)
-);
+  ID          INT(8) NOT NULL AUTO_INCREMENT,
+  username    VARCHAR(15) NOT NULL,
 
-CREATE TABLE IF NOT EXISTS messages (
-  ID          INT(10),
-  User_ID     INT(8),
-  message     VARCHAR(140),
-  Room_ID     INT(8),
-  createdAt   TIMESTAMP
+  PRIMARY KEY (ID)
 );
 
 CREATE TABLE IF NOT EXISTS rooms (
-  ID          INT(8),
-  name        VARCHAR(30)
+  ID          INT(8) NOT NULL AUTO_INCREMENT,
+  name        VARCHAR(30) NOT NULL,
+
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  ID          INT(10) NOT NULL AUTO_INCREMENT,
+  User_ID     INT(8) NOT NULL,
+  message     VARCHAR(140) NOT NULL,
+  Room_ID     INT(8) NOT NULL DEFAULT 1,
+  createdAt   TIMESTAMP,
+
+  PRIMARY KEY (ID),
+
+  FOREIGN KEY (User_ID)
+    REFERENCES users(ID)
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (Room_ID)
+    REFERENCES rooms(ID)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS friendsOf (
-  User_ID     INT(8),
-  Friend_ID   INT(8)
+  User_ID     INT(8) NOT NULL,
+  Friend_ID   INT(8) NOT NULL,
+
+  FOREIGN KEY (User_ID)
+    REFERENCES users(ID)
+    ON DELETE CASCADE,
+
+  FOREIGN KEY (Friend_ID)
+    REFERENCES users(ID)
+    ON DELETE CASCADE
 );
 
 /* Create other tables and define schemas for them here! */
